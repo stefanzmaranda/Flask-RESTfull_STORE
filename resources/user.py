b030011b07482dc438911d24964cc6fea0a14627
Stefan Zmaranda <stefanzmaranda@yahoo.com>
2022-06-06 15:26:34 +0300

    change function to find by name instead of id

--- resources/user.py
+++ resources/user.py
@@ -19,7 +19,7 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
-        if UserModel.find_by_id(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {'message':'User already existing !'},400
 
         user = UserModel(**data)
