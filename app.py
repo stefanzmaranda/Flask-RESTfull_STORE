c1d6acafd224d8f3d7566709c5e942b675856169
Stefan Zmaranda <stefanzmaranda@yahoo.com>
2022-06-07 11:23:56 +0300

    Changing name of the postgress DB to be accepted by SQLAlchemy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import  Api
@@ -9,8 +10,14 @@ from resources.user import UserRegister
 from resources.item import Item,ItemList
 from resources.store import Store,StoreList
 
+
+#Flask SQLAlchemy doesn`t support postgress DB anymore only postgresql !
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri,'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
